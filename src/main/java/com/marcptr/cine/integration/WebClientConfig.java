231519a75903341fddffc3918ab8585c37f2c467
marcphtr <marcphtr@gmail.com>
2026-06-05 23:12:10 +0200

    updated

--- src/main/java/com/marcptr/cine/integration/WebClientConfig.java
+++ src/main/java/com/marcptr/cine/integration/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.marcptr.cine.config;
+package com.marcptr.cine.integration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
